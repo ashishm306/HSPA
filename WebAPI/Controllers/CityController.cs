062499e83694c52db021a953f06ef9389e9b66e7
ashish <ashishm306gmail.com>
2023-10-02 22:53:39 +0530

    added webapi

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CityController : Controller
+    {   
+       [HttpGet]
+       public IEnumerable<string> Get()
+        {
+            return new string[] { "Atlanta", "New York" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atlanta";
+        }
+    }
+}
